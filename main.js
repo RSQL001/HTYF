8d88ee29de172ed80322b1530314153426e5b350
RSQL <wizardman001@gmail.com>
2020-08-14 16:10:23 +0100

    Pleasee

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 
       $(document).ready(function() { 
         $('.slider').slick({
-          dots: true,
-          slidesToShow: 1,
+          dots: true, 
+          arrows: true, 
+          infinite: true, 
+          slidesToShow: 1, 
+          slidesToScroll: 1, 
           autoplay: true,
           autoplaySpeed: 1000
         });
