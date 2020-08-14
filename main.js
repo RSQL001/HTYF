3c0d83115dd50f40aeb9c257ec9ecf89904ff2d8
RSQL <wizardman001@gmail.com>
2020-08-14 16:52:17 +0100

    NN

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 
-      $(document).ready(function() { 
-        $('.slider').slick({
-          dots: true, 
-          arrows: true, 
-          infinite: true, 
-          slidesToShow: 1, 
-          slidesToScroll: 1, 
-          autoplay: true,
-          autoplaySpeed: 1000
-        });
-      });
+      
